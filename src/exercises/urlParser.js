a16a0ce0818a4d0fe31b3971356e8824610f6c78
Hernan Faifman <hernan.faifman@experoinc.com>
2024-04-26 20:32:57 -0300

    added challenge

---
+++ src/exercises/urlParser.js
@@ -0,0 +1,56 @@
+const splitString = (str, splitSymbol) => str.split(splitSymbol)
+
+const getQueryParams = (url) => {
+    const splittedUrl = splitString(url, '?')
+    const [baseUrl, queryParams] = splittedUrl
+
+    return { baseUrl, queryParams }
+}
+
+const getProperValue = (input) => {
+    const isValueABoolean = input === 'true' || input === 'false'
+
+    if (isValueABoolean) return input === 'true'
+
+    const isValueANumber = isNaN(Number(input)) === false
+
+    if (isValueANumber) return Number(input)
+
+    return input
+}
+
+function paramsToObject(queryParams) {
+    const result = {}
+    const urlParams = new URLSearchParams(queryParams)
+
+    for (const [key, value] of urlParams) {
+        result[key] = getProperValue(value)
+    }
+
+    return result
+}
+
+const getHash = (format, instance) => {
+    const values = splitString(format, '/')
+    const { baseUrl, queryParams } = getQueryParams(instance)
+    const instanceValues = splitString(baseUrl, '/')
+
+    const hash = values.reduce((acc, curr, index) => {
+        const isValueADynamicValue = curr.includes(':')
+        if (isValueADynamicValue) {
+            const normalisedValue = curr.replace(':', '')
+            const param = instanceValues[index]
+            acc[normalisedValue] = getProperValue(param)
+        }
+        return acc
+    }, {})
+
+    if (queryParams) {
+        const queryParamsObj = paramsToObject(queryParams)
+        return { ...hash, ...queryParamsObj }
+    }
+
+    return hash
+}
+
+export default getHash
