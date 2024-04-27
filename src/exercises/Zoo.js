c76a02571f5bc9791f307ff1bd792adfa504e982
Hernan Faifman <hernan.faifman@experoinc.com>
2024-04-26 23:03:31 -0300

    added formatting to exercises

--- src/exercises/Zoo.js
+++ src/exercises/Zoo.js
@@ -1,70 +1,70 @@
 class Validator {
-    checkParamPresenceAndValidType = ({ param, type, errorMessageType }) => {
-        if (!param) {
-            throw Error(`${errorMessageType} is needed`)
-        } else if (typeof param !== type) {
-            throw Error(`${errorMessageType} is not of type ${type}`)
-        }
+  checkParamPresenceAndValidType = ({ param, type, errorMessageType }) => {
+    if (!param) {
+      throw Error(`${errorMessageType} is needed`);
+    } else if (typeof param !== type) {
+      throw Error(`${errorMessageType} is not of type ${type}`);
     }
+  };
 }
 
 class Animal {
-    constructor(type, sound) {
-        const validator = new Validator()
-        validator.checkParamPresenceAndValidType({
-            param: type,
-            type: 'string',
-            errorMessageType: 'type',
-        })
-        validator.checkParamPresenceAndValidType({
-            param: sound,
-            type: 'string',
-            errorMessageType: 'sound',
-        })
-        this.type = type
-        this.sound = sound
-    }
+  constructor(type, sound) {
+    const validator = new Validator();
+    validator.checkParamPresenceAndValidType({
+      param: type,
+      type: "string",
+      errorMessageType: "type",
+    });
+    validator.checkParamPresenceAndValidType({
+      param: sound,
+      type: "string",
+      errorMessageType: "sound",
+    });
+    this.type = type;
+    this.sound = sound;
+  }
 
-    speak(speech) {
-        const validator = new Validator()
-        validator.checkParamPresenceAndValidType({
-            param: speech,
-            type: 'string',
-            errorMessageType: 'speech',
-        })
-        const splitSpeech = speech.split(' ')
-        return splitSpeech.map((phrase) => `${phrase} ${this.sound} `).join('')
-    }
+  speak(speech) {
+    const validator = new Validator();
+    validator.checkParamPresenceAndValidType({
+      param: speech,
+      type: "string",
+      errorMessageType: "speech",
+    });
+    const splitSpeech = speech.split(" ");
+    return splitSpeech.map((phrase) => `${phrase} ${this.sound} `).join("");
+  }
 }
 
 class Lion extends Animal {
-    constructor() {
-        super('lion', 'roar')
-    }
+  constructor() {
+    super("lion", "roar");
+  }
 }
 
 class Tiger extends Animal {
-    constructor() {
-        super('tiger', 'grrr')
-    }
+  constructor() {
+    super("tiger", "grrr");
+  }
 }
 
 class Zoo {
-    constructor() {
-        this.animals = [new Lion(), new Tiger()]
-    }
+  constructor() {
+    this.animals = [new Lion(), new Tiger()];
+  }
 
-    addNewAnimal(animal) {
-        if (animal instanceof Animal) {
-            this.animals = [...this.animals, animal]
-            return this.animals
-        }
-        throw Error('not a valid animal')
+  addNewAnimal(animal) {
+    if (animal instanceof Animal) {
+      this.animals = [...this.animals, animal];
+      return this.animals;
     }
+    throw Error("not a valid animal");
+  }
 
-    getAnimals() {
-        return this.animals
-    }
+  getAnimals() {
+    return this.animals;
+  }
 }
 
-export default Zoo
+export default Zoo;
